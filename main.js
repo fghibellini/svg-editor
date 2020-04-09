d21698bac3c514d90bccf56545b58abf8fd42b96
Filippo Ghibellini <filippo.ghibellini@zalando.de>
2020-04-10 01:06:06 +0200

    Line from point to point

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const canvas = document.getElementById("canvas");
 
 let isPressed = false;
 const points = [];
+const segments = [];
 let current = null;
-let handlePointA = null;
-let handlePointB = null;
-let handleLine = null;
+let handlePointA = addPoint({ x: 0, y: 0 });
+let handlePointB = addPoint({ x: 0, y: 0 });
+let handleLine = addLine({ x1: 0, y1: 0, x2: 0, y2: 0 });
 
 function addPoint(p) {
   const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -30,40 +31,53 @@ function addLine(p) {
   return line;
 }
 
-function bezierFocus(p) {
-}
-
 canvas.addEventListener("mouseup", evt => {
-  //if (isMouseDown) {
+  if (isPressed) {
     const { x, y } = evt;
-    addPoint({ x, y });
-  //}
+    isPressed = false;
+    handleLine.setAttribute("visibility", "hidden");
+    handlePointA.setAttribute("visibility", "hidden");
+    handlePointB.setAttribute("visibility", "hidden");
+  }
 }, true);
 
 canvas.addEventListener("mousemove", evt => {
   if (isPressed) {
     const { x, y } = evt;
-    // handle A
-    handlePointA.setAttribute("cx", x);
-    handlePointA.setAttribute("cy", y);
-    // handleB
-    const pB = { x: current.x - (x - current.x), y: current.y - (y - current.y) };
-    handlePointB.setAttribute("cx", pB.x);
-    handlePointB.setAttribute("cy", pB.y);
-    // handle-line
-    handleLine.setAttribute("x1", x);
-    handleLine.setAttribute("y1", y);
-    handleLine.setAttribute("x2", pB.x);
-    handleLine.setAttribute("y2", pB.y);
+    updateHandles({ x, y });
   }
 }, true);
 
+function updateHandles({ x, y}) {
+  // handle A
+  handlePointA.setAttribute("cx", x);
+  handlePointA.setAttribute("cy", y);
+  // handleB
+  const pB = { x: current.x - (x - current.x), y: current.y - (y - current.y) };
+  handlePointB.setAttribute("cx", pB.x);
+  handlePointB.setAttribute("cy", pB.y);
+  // handle-line
+  handleLine.setAttribute("x1", x);
+  handleLine.setAttribute("y1", y);
+  handleLine.setAttribute("x2", pB.x);
+  handleLine.setAttribute("y2", pB.y);
+}
+
 canvas.addEventListener("mousedown", evt => {
   isPressed = true;
+  handleLine.setAttribute("visibility", "visible");
+  handlePointA.setAttribute("visibility", "visible");
+  handlePointB.setAttribute("visibility", "visible");
   const { x, y } = evt;
   addPoint({ x, y });
   current = { x, y };
-  handlePointA = addPoint({ x, y });
-  handlePointB = addPoint({ x, y });
-  handleLine = addLine({ x1: x, y1: y, x2: x, y2: y });
+  points.push(current);
+  updateHandles({ x, y });
+  if (points.length > 1) {
+    const prev = points[points.length - 2];
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", `M ${prev.x} ${prev.y} L ${x} ${y}`);
+    path.setAttribute("stroke", "pink");
+    canvas.appendChild(path);
+  }
 }, true);
