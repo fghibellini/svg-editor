206398f57cb7665295b7107de59e0f25f1479bc1
Filippo Ghibellini <filippo.ghibellini@zalando.de>
2020-04-10 15:51:15 +0200

    Handle moving

--- main.js
+++ main.js
@@ -21,6 +21,11 @@ onResize();
 
 const MIN_MOVEMENT = 3;
 
+const HS = {
+  Left: "<left>",
+  Right: "<right>"
+};
+
 // editor state
 const objects = [];
 let bezierState = {
@@ -30,13 +35,14 @@ let bezierState = {
   handlePointA: addPoint({ x: 0, y: 0 }),
   handlePointB: addPoint({ x: 0, y: 0 }),
   handleLine: addLine({ x1: 0, y1: 0, x2: 0, y2: 0 }),
-  clickingPoint: null,
-  clickingPointStartingCoords: null,
-  clickingPointWasMoved: false
+  clickedPoint: null,
+  clickedPointStartingCoords: null,
+  clickedPointWasMoved: false,
+  clickedHandle: null
 };
 
 canvas.addEventListener("mouseup", evt => {
-  const { drawingBezier, isPressed, points, clickingPoint, clickingPointWasMoved } = bezierState;
+  const { drawingBezier, isPressed, points, clickedPoint, clickedPointWasMoved, clickedHandle } = bezierState;
   if (isPressed) {
     console.log("mouseup is pressed");
     const { x, y } = evt;
@@ -45,12 +51,14 @@ canvas.addEventListener("mouseup", evt => {
     //current.$hdl_line.setAttribute("visibility", "hidden");
     //current.$rgt_hdl.setAttribute("visibility", "hidden");
     //current.$lft_hdl.setAttribute("visibility", "hidden");
-  } else if (clickingPoint && clickingPointWasMoved) {
-    bezierState.clickingPoint = null;
-  } else if (clickingPoint && clickingPoint === points[0]) {
+  } else if (clickedHandle) {
+    bezierState.clickedHandle = null;
+  } else if (clickedPoint && clickedPointWasMoved) {
+    bezierState.clickedPoint = null;
+  } else if (clickedPoint && clickedPoint === points[0]) {
     console.log("mouseup clicking point");
-    const t = clickingPoint;
-    points.push(t);
+    const t = clickedPoint;
+    points.push(t); // TODO don't add point twice, have a closed flag instead
     const prev = points[points.length - 2];
     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute("d", `M ${prev.x} ${prev.y} C ${prev.x + prev.hx} ${prev.y + prev.hy} ${t.x - t.hx} ${t.y - t.hy} ${t.x} ${t.y}`);
@@ -60,7 +68,7 @@ canvas.addEventListener("mouseup", evt => {
     bezierState.drawingBezier = false;
     objects.push({ isClosed: true, points }); // TODO map points
     bezierState.points = [];
-    bezierState.clickingPoint = null;
+    bezierState.clickedPoint = null;
   } else {
     // TODO highlight handles ?
   }
@@ -74,7 +82,7 @@ function computeDistance(p1, p2) {
 
 canvas.addEventListener("mousemove", evt => {
   const { x, y } = evt;
-  const { isPressed, points, clickingPoint, clickingPointWasMoved, clickingPointStartingCoords } = bezierState;
+  const { isPressed, points, clickedPoint, clickedPointWasMoved, clickedPointStartingCoords, clickedHandle } = bezierState;
   console.log("mousemove");
   if (isPressed) {
     console.log("is pressed");
@@ -82,12 +90,22 @@ canvas.addEventListener("mousemove", evt => {
     current.hx = x - current.x;
     current.hy = y - current.y;
     onHandleChange(current);
-  } else if (clickingPoint) {
-    if (clickingPointWasMoved || computeDistance(evt, clickingPointStartingCoords) > MIN_MOVEMENT) {
-      bezierState.clickingPointWasMoved = true;
-      clickingPoint.x = x;
-      clickingPoint.y = y;
-      onHandleChange(clickingPoint);
+  } else if (clickedHandle) {
+    const point = clickedHandle.point;
+    if (clickedHandle.side == HS.Right) {
+      point.hx = x - point.x;
+      point.hy = y - point.y;
+    } else {
+      point.hx = point.x - x;
+      point.hy = point.y - y;
+    }
+    onHandleChange(point);
+  } else if (clickedPoint) {
+    if (clickedPointWasMoved || computeDistance(evt, clickedPointStartingCoords) > MIN_MOVEMENT) {
+      bezierState.clickedPointWasMoved = true;
+      clickedPoint.x = x;
+      clickedPoint.y = y;
+      onHandleChange(clickedPoint);
     }
   } else {
     console.log("nothing");
@@ -119,11 +137,15 @@ function onHandleChange(p) {
 
 canvas.addEventListener("mousedown", evt => {
   const t = evt.target._Z_point;
+  const r = evt.target._Z_handle;
   if (t) {
-    console.log("setting clickingPoint");
-    bezierState.clickingPoint = t;
-    bezierState.clickingPointWasMoved = false;
-    bezierState.clickingPointStartingCoords = { x: evt.x, y: evt.y };
+    console.log("setting clickedPoint");
+    bezierState.clickedPoint = t;
+    bezierState.clickedPointWasMoved = false;
+    bezierState.clickedPointStartingCoords = { x: evt.x, y: evt.y };
+  } else if (r) {
+    console.log("setting clickedHandle");
+    bezierState.clickedHandle = r;
   } else {
     const { x, y } = evt;
     const { drawingBezier, isPressed, points } = bezierState;
@@ -137,6 +159,8 @@ canvas.addEventListener("mousedown", evt => {
     const $el = addPoint({ x, y });
     const current = { x, y, hx: 0, hy: 0, $el, $lft_seg: null, $lft_hdl, $rgt_hdl, $hdl_line, prev: points.length > 0 ? points[points.length - 1] : null, next: null }
     $el._Z_point = current;
+    $lft_hdl._Z_handle = { point: current, side: HS.Left };
+    $rgt_hdl._Z_handle = { point: current, side: HS.Right };
     if (points.length > 0) {
       points[points.length - 1].next = current;
     }
